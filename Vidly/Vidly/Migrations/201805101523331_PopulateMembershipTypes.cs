1bfaf12d03554e910005686f929e3c8a1601101e
John Snable <helwyr392@gmail.com>
2018-05-15 15:38:18 -0400

    Change date format on Customers/Details view.

---
+++ Vidly/Vidly/Migrations/201805101523331_PopulateMembershipTypes.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (SignUpFee, DurationInMonths, DiscountRate) " +
+                "VALUES (0, 0, 0), (30, 1, 10), (90, 3, 15), (300, 12, 20)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Vidly/Vidly/Migrations/201805101634126_PopulateGenres.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Name) VALUES ('Action'), " +
+                "('Comedy'), ('Family'), ('Romance'), ('Thriller')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
