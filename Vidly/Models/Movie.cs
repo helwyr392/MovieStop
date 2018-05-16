441f295ae69c6a3cc64e0ecf95a027f6e9f0f9c2
John Snable <helwyr392@gmail.com>
2018-05-16 12:08:53 -0400

    Add Display Name to ReleasedDate and DateAdded in Movie model.

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,13 +9,21 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleasedDate { get; set; }
+
+        [Display(Name = "Date Added")]
         public DateTime DateAdded { get; set; }
+
         public Genre Genre { get; set; }
+
         public int GenreID { get; set; }
+
         public int NumberInStock { get; set; }
     }
 }
