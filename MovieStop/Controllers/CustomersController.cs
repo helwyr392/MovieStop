a84aa2408de09332b870983b01d3b9bd91944088
John Snable <helwyr392@gmail.com>
2018-05-22 16:17:02 -0400

    Update membership dropdown labelfor to point to MembershipTypeID, not MembershipType.  Remove value from birthdate check on Customers/Details view.

--- MovieStop/Controllers/CustomersController.cs
+++ MovieStop/Controllers/CustomersController.cs
@@ -75,16 +75,16 @@ namespace MovieStop.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
-            if (!ModelState.IsValid)
-            {
-                var viewModel = new CustomerFormViewModel
-                {
-                    Customer = customer,
-                    MembershipTypes = _context.MembershipTypes.ToList()
-                };
+            //if (!ModelState.IsValid)
+            //{
+            //    var viewModel = new CustomerFormViewModel
+            //    {
+            //        Customer = customer,
+            //        MembershipTypes = _context.MembershipTypes.ToList()
+            //    };
 
-                return View("CustomerForm", viewModel);
-            }
+            //    return View("CustomerForm", viewModel);
+            //}
 
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
