1e5232b7b1cd1061637669a1292c298c52bdff2b
John Snable <helwyr392@gmail.com>
2018-05-25 10:38:23 -0400

    Add CustomersController API, and Customer DTO.

---
+++ MovieStop/Dtos/CustomerDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MovieStop.Models;
+
+namespace MovieStop.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        [Required]
+        public int MembershipTypeID { get; set; }
+
+        [Required]
+        [Min18YearsIfMember]
+        public DateTime Birthdate { get; set; }
+    }
+}
