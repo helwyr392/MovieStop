693486d363a46153bab8b1b7a6d147249066b7d2
John Snable <helwyr392@gmail.com>
2018-06-09 20:23:22 -0400

    Add Rental model, NewRentals API Controller, and NewRentalDTO.

---
+++ MovieStop/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,39 @@
+﻿using MovieStop.Dtos;
+using MovieStop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieStop.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        // Constructor
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: /api/NewRental
+        public IHttpActionResult GetNewRentals()
+        {
+            return Ok("GET: /api/NewRentals");
+        }
+
+        // POST: /api/NewRental
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT
+
+        // DELETE
+    }
+}
