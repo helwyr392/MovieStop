a15e9d2970d430a8cf3829ffdf6ed0b839ed8cea
John Snable <helwyr392@gmail.com>
2018-06-09 20:38:10 -0400

    Add POST to NewRentalsController API controller.

--- MovieStop/Controllers/Api/NewRentalsController.cs
+++ MovieStop/Controllers/Api/NewRentalsController.cs
@@ -29,7 +29,26 @@ namespace MovieStop.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    RentedDate = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            // Save changes
+            _context.SaveChanges();
+
+            // Return
+            return Ok();
         }
 
         // PUT
